013a4d3910cde47cdc96e41d72bc058fad74baa2
Benjamin <thiebaut.benjamin@live.fr>
2019-01-14 11:04:19 +0100

    Changement
    Utilisation des fichier en locale
    Ajout traduction interface

--- PatchFrYgopro/Form1.cs
+++ PatchFrYgopro/Form1.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using PatchFrYgopro.Properties;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Net;
 using System.Text;
@@ -25,11 +27,32 @@ namespace PatchFrYgopro
             if (result == DialogResult.OK)
             {
                 string files = openFileDialog1.FileName;
-                using (WebClient client = new WebClient())
+                File.Copy("./Resources/cards.cdb", files.Replace("ygopro_vs_links.exe", "") + @"cards.cdb",true);
+                File.Copy("./Resources/config", files.Replace("ygopro_vs_links.exe", "") + @"expansions\live2017links\.git\config",true);
+
+                var frFile = File.ReadAllLines("./Resources/strings.conf");
+                var frList = new List<string>(frFile).Where(w => w.StartsWith("!")).ToList();
+                var enFile = File.ReadAllLines(files.Replace("ygopro_vs_links.exe", "") + @"strings.conf");
+                var enList = new List<string>(enFile).Where(w => w.StartsWith("!")).ToList();
+                var listFinal = new List<string>();
+                listFinal.Add("#The first line is used for comment");
+                Console.WriteLine(@"Trans strings.conf");
+                foreach (var text in enList)
                 {
-                    client.DownloadFile(new Uri("http://ygomanager.diypiandco.com/ygomanager/upload/Percy/cards.cdb"), files.Replace("ygopro_vs_links.exe", "")+ @"cards.cdb");
-                    client.DownloadFile(new Uri("http://ygomanager.diypiandco.com/ygomanager/upload/Percy/config"), files.Replace("ygopro_vs_links.exe", "")+ @"expansions\live2017links\.git\config");
+                    var temp = text.Split(' ')[1];
+                    var textAdd = "";
+                    var index = frList.FindIndex(w => w.Split(' ')[1] == text.Split(' ')[1]);
+                    if (index>=0)
+                    {
+                        textAdd = frList[index];
+                    }
+                    else
+                    {
+                        textAdd =text ;
+                    }
+                    listFinal.Add(textAdd);
                 }
+                File.WriteAllLines(files.Replace("ygopro_vs_links.exe", "") + @"strings.conf", listFinal.ToArray());
             }
             MessageBox.Show("Patch fini :) !");
         }
